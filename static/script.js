81ee302e144ee9148935b748d321543654db1363
a\김동은 <no4731@gmail.com>
2025-03-29 15:28:17 +0900

    DB 오류가 나서 ,를 추가해서 수정

--- static/script.js
+++ static/script.js
@@ -2,11 +2,11 @@
 const studentData = [
   { id: "202301", name: "김민수", googleId: "kms01@gmail.com", googlePw: "pass1234" },
   { id: "202302", name: "이서연", googleId: "lsy02@gmail.com", googlePw: "pass2345" },
-  { id: "202303", name: "박지후", googleId: "pjh03@gmail.com", googlePw: "pass3789" }  
-  { id: "202304", name: "홍길동", googleId: "pjh03@gmail.com", googlePw: "pass3411" }  
-  { id: "202305", name: "김철수", googleId: "pjh03@gmail.com", googlePw: "pass3400" }  
-  { id: "202306", name: "이영희", googleId: "pjh03@gmail.com", googlePw: "pass3477" }  
-  { id: "202307", name: "안철수", googleId: "pjh03@gmail.com", googlePw: "pass3445" }  
+  { id: "202303", name: "박지후", googleId: "pjh03@gmail.com", googlePw: "pass3789" },  
+  { id: "202304", name: "홍길동", googleId: "pjh03@gmail.com", googlePw: "pass3411" },  
+  { id: "202305", name: "김철수", googleId: "pjh03@gmail.com", googlePw: "pass3400" },  
+  { id: "202306", name: "이영희", googleId: "pjh03@gmail.com", googlePw: "pass3477" },  
+  { id: "202307", name: "안철수", googleId: "pjh03@gmail.com", googlePw: "pass3445" },  
 ];
 
 // 폼과 요소 참조
@@ -29,6 +29,7 @@ form.addEventListener('submit', function (e) {
     // 결과 표시
     googleIdSpan.textContent = student.googleId;
     googlePwSpan.textContent = student.googlePw;
+
   } else {
     // 조회 실패 메시지
     googleIdSpan.textContent = "조회 결과가 없습니다.";
